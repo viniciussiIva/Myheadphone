7fe37156977fac3cd6434545c33d81dfe65ae105
Vinícius Silva <vinicius_vsom@outlook.com>
2022-03-21 22:11:26 -0300

    Update and rename assets/js/main.js to bens/js/main.js

--- bens/js/main.js
+++ bens/js/main.js
@@ -4,7 +4,7 @@ const navMenu = document.getElementById('nav-menu'),
       navToggle = document.getElementById('nav-toggle'),
       navClose = document.getElementById('nav-close')
 
-/*===== MENU SHOW =====*/
+
 /* VALIDANDO O IF */
 if (navToggle) {
   navToggle.addEventListener('click', () => {
